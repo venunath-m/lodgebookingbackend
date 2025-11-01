4316aba3f626e2472aa5f3e1694f0a4a0c5cd821
venunath-m <venunathm30@gmail.com>
2025-11-01 22:17:39 +0530

    ew

--- accounting/models.py
+++ accounting/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean ,ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from database import Base
